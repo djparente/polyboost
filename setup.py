547028171a619888d138b65367f7a2dd55759270
Daniel J. Parente <dparente@kumc.edu>
2019-11-01 19:15:32 +0100

    Add setuptools information and a gitignore file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polyboost", # Replace with your own username
+    version="0.0.1",
+    author="Daniel J. Parente",
+    author_email="dparente@kumc.edu",
+    description="An enhanced genomic classifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/djparente/polyboost",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'xgboost'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
