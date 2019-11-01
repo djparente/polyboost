ca46afd6305375352de02d0869574b6b1208de0a
Daniel Parente <dparente@kumc.edu>
2019-11-01 18:42:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# PolyBoost
+
+## Description
+**PolyBoost** is a post-analysis tool for the batch processing output of [PolyPhen-2](http://genetics.bwh.harvard.edu/pph2/) (I am not affiliated with the PolyPhen-2 group) that replaces the [naive Bayes classifier](https://en.wikipedia.org/wiki/Naive_Bayes_classifier) with an extreme gradient boosting [XGBoost]([https://github.com/dmlc/xgboost) classifier.
+## Citation
+
+**PolyBoost: An enhanced genomic variant classifier** is currently under peer review. If, for some reason, you need to cite this in the meantime, please contact me.
+
+## Usage
+
+You will need to install PolyBoost (see below) and obtain the **batch mode output** from PolyPhen2 predictions (http://genetics.bwh.harvard.edu/pph2/bgi.shtml). An example of the batch mode output is found in polyphen2-example.txt in this repository.
+
+Run PolyBoost as follows:
+
+    python3 -m polyboost [PolyPhen2 Output File] [Classifier]
+
+Where [PolyPhen2 Output File] is the path to the **batch mode** output from PolyPhen-2 and Classifier is either **humdiv** or **humvar**. If you don't know which one to use, use **humvar**.   
+
+## Set-up
+
+### Requirements
+PolyBoost requires Python 3.7, xgboost, numpy and scipy. Numpy and SciPy should be installed automatically as dependencies of PolyBoost and XGBoost. ***XGBoost will **not** be automatically installed because installation from PyPi does not work reliably on Windows at time of release. Install XGBoost before installing PolyBoost.***
+
+Use of a Python [virtualenv](https://docs.python.org/3/library/venv.html) is recommended, but not required.
+
+
+#### Python 3.7
+
+You will need to install Python 3.7 through standard methods. 
+#### XGBoost
+
+Installation of XGBoost is ostensibly as easy as:
+
+    pip3 install xgboost
+
+I found, however, that I could not download this from the PyPi repository on Windows. In this case, you can download the XGBoost python wheel from [this location](https://www.lfd.uci.edu/~gohlke/pythonlibs/#xgboost) and install it like:
+
+    pip3 install xgboost-{version}-{pythonversion}-{architecture}.whl
+
+Example: I installed XGBoost 0.90 for Python 3.7 (32-bit), so I used:
+
+    pip3 install xgboost-0.90-cp37-cp37m-win32.whl
+
+I used the 32-bit module even though I am using 64-bit Windows because Python 3.7 was installed in 32 bit mode on my computer.
+
+If you have difficulty, detailed instructions for installing XGBoost for your platform can be found here: https://xgboost.readthedocs.io/en/latest/build.html
+
+#### PolyBoost
+
+Install PolyBoost with:
+
+    pip install polyboost
+    
+#### Numpy and Scipy
+
+You should **not** need to install numpy and scipy manually, but you can do so with:
+
+    pip install numpy scipy
+
+## Options
+
+### Number of Threads (--threads)
+You can specify the number of threads to run predictions. You must choose between 1 and 16 threads. If you make no selection, the default is to use 4 threads.
+
+Example using 8 threads:
+
+    python3 -m polyboost polyphen2output.txt humvar --threads 8
+
+### Threshold (--threshold)
+You can manually choose a threshold between binary classification of "benign" and "damaging". The default choices are 0.504057 for HumVar and 0.4250919 for HumDiv. These defaults were determined during classifier development by maximizing the Youden index (sensitivity + specificity - 1) of the receiver operating characteristic (ROC) curve.
+
+Example using a threshold value of 0.25:
+
+    python3 -m polyboost polyphen2output.txt humvar --threshold 0.25 
+
+### Output (--out)
+By default, PolyBoost outputs to the console (standard output). You can optionally output to a file using --out.
+
+Example redirecting to output.txt
+
+    python3 -m polyboost polyphen2output.txt humvar --out output.txt
+
+### 
+
+## Questions?
+
+Please e-mail me with questions. I will do my best to respond.
---
+++ polyboost/polyboost.py
@@ -0,0 +1,142 @@
+# Daniel J. Parente, MD PhD
+# University of Kansas Medical Center
+# 2019-10-17
+
+import xgboost, argparse, sys, io
+import numpy as np
+import pkg_resources
+
+# Get the fields from a line that is read, split by tabs and deleting newlines and spaces
+def getFields(line):
+    fields = line.replace("\r\n","").replace("\n","").replace(" ","").split("\t")
+    return fields
+
+# Parse a PolyPhen file to extract the features
+def parsePolyPhen(inputfile):
+    data = inputfile.readlines()
+
+    # Header line
+    headerFields = getFields(data[0][1:]) #Parse the header, ignoring the initial #
+    headers = headerFields[0:4] + ['polyboost_probability','polyboost_prediction']
+
+    headerToIndex = { x : i for i, x in enumerate(headerFields) }
+    vectors = []
+    identifiers = []
+    for line in data[1:]:
+        # Ignored commented lines
+        if line.replace(" ","").startswith('#'):
+            continue
+
+        # Gets all the fields
+        fields = getFields(line)
+        identifiers.append(fields[0:4])
+
+        # Compatibility with training files: Training files with "missing" features for the last few features
+        # of a row omit the tabs; put these back in as missing data
+        if len(fields) < len(headerFields):
+            fields = fields + [''] * (len(headerFields) - len(fields))
+
+        # Get the data points of interest
+        v = [
+            fields[headerToIndex['Score1']], #Float 0
+            fields[headerToIndex['dScore']], #Float 1
+            fields[headerToIndex['Nobs']], # Int 2
+            fields[headerToIndex['dVol']], # Float 3
+            fields[headerToIndex['PfamHit']], # Int 4 (Will need special handling)
+            fields[headerToIndex['IdPmax']], # Float 5
+            fields[headerToIndex['IdQmin']], # Float 6
+            fields[headerToIndex['CpG']], # Int 7
+            fields[headerToIndex['NormASA']], #Float 8
+            fields[headerToIndex['B-fact']] ,#Float 9
+            fields[headerToIndex['dProp']]  # Float 10
+            ]
+
+        # Convert the floats:
+        for i in [0,1,3,5,6,8,9,10]:
+            if v[i] == '?' or v[i] == "":
+                v[i] = float('nan')
+            else:
+                v[i] = float(v[i])
+
+        # Convert the ints
+        for i in [2,7]:
+            if v[i] == '?' or v[i] == "":   # PolyPhen-2 produces '?' but training files have whitespace-only fields (stripped out in proprocessing)
+                v[i] = None
+            else:
+                v[i] = int(v[i])
+
+        # Convert PfamHit into a boolean: When absent this results in "NO" being output by PolyPhen2, which we
+        # will interpret as 0 (False). Any other string except ? we will interpret as 1 (True). I am not sure
+        # if it will ever output "?", but we will treat this as missing data.
+        # Convert the ints
+        for i in [4]: # Yes, I know the loop over 1 element is inefficient coding, but it matches the formatting above; readability!
+            if v[i] == '?' or v[i] == "":
+                v[i] = None
+            else:
+                v[i] = 0 if v[i] == "NO" else 1
+        vectors.append(v)
+
+    # Convert into an xgboost DMatrix (via NumPy array)
+    npa = np.array(vectors)
+    matrix = xgboost.DMatrix(npa)
+
+    # Return the matrix and the identifiers (Protein and Variant specificiation)
+    return matrix, identifiers, headers
+
+def main():
+    # Initialize an argument parsers
+    parser = argparse.ArgumentParser(description="PolyBoost applies an extreme gradient boosted classifier to the PolyPhen-2 feaature set using a classifier based on either the HumVar or HumDiv training datasets.")
+
+    parser.add_argument('--threads', help="Number of threads for the classifier", type=int, choices=range(1,17))
+    parser.add_argument('--threshold', help="Custom threshold value for binary prediction", type=float)
+    parser.add_argument('inputfile', type=argparse.FileType('r'), help="PolyPhen-2 Batch Processing Output File")
+    parser.add_argument('classifier', metavar="classifier", choices=["humvar", "humdiv"],
+                        help="Select the classifier to use; either humvar or humdiv")
+    parser.add_argument('--out', help="Output file", type=argparse.FileType('w'), default=sys.stdout)
+
+    # Read the arguments
+    args = parser.parse_args()
+
+    # Extract an XGBoost DMatrix and identifiers (protein and variant descriptions) from the file
+    matrix, identifiers, header = parsePolyPhen(args.inputfile)
+
+    # Initialize a (multithreaded) booster
+    threads = 4
+    if args.threads is not None:
+        threads = args.threads
+
+    # Initialize a classifier
+    bst = xgboost.Booster({'nthread': threads})
+
+    # Load the appropriate model and threshold
+    threshold = None
+    if args.classifier == "humvar":
+        modelpath = pkg_resources.resource_filename("models", "humvar-final.model")
+        bst.load_model(modelpath)
+        threshold = 0.504057
+    elif args.classifier == "humdiv":
+        modelpath = pkg_resources.resource_filename("models", "humdiv-final.model")
+        bst.load_model(modelpath)
+        threshold = 0.4250919
+    else:
+        raise("Invalid classifier")
+
+    # Set a custom threshold if any
+    if args.threshold is not None:
+        threshold = args.threshold
+
+    # Perform predictions to obtain probabilities of damaging
+    pred = 1-bst.predict(matrix)    # The probability of damaging is actually actually 1- the model's probability due to the way it was trained
+
+    # Apply a threshold to produce a binary prediction
+    binary_predictions = [ "damaging" if x > threshold else "benign" for x in pred ]
+
+    # Write the header
+    print("\t".join(header), file=args.out)
+
+    # Write out the identifiers (protein and variant specificiation), numerical probability of damaging variant and binary prediction
+    for x,y,z in zip(identifiers, pred, binary_predictions):
+        print("\t".join(x + [str(y), z]), file=args.out)
+
+if __name__ == '__main__':
+    main()
