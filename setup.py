72be92c788e470983a1b38fe101e92e830f644b6
Daniel J. Parente <dparente@kumc.edu>
2019-11-02 21:30:18 +0100

    Recalculate final models

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="polyboost", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Daniel J. Parente",
     author_email="dparente@kumc.edu",
-    description="An enhanced genomic classifier",
+    description="An enhanced genomic variant classifier",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/djparente/polyboost",
